b018f7fae49921b9daf57cc94c9dd1dfda0b5802
diego64 <diegoferreira1964@gmail.com>
2020-06-17 14:37:48 -0300

    Novas funcionalidades

--- router.ts
+++ router.ts
@@ -1,5 +1,5 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getUsers, getUser, addUser } from './controller/user.ts';
+import { getUsers, getUser, addUser, updateUser, deleteUser } from './controller/user.ts';
 
 const router = new Router();
 
@@ -10,5 +10,7 @@ router.get("/", (ctx) => {
 router.get("/users", getUsers);
 router.get("/user/:id", getUser);
 router.post("/user", addUser);
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser)
 
 export default router;
